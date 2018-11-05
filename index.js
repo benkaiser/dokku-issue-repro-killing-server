8db5a7e0e3c759ee2f1ab3a0fe3277374109a7c1
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2018-11-04 18:22:35 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (_, res) => res.send('Hello World!'))
+
+app.get('/die', () => {
+  process.exit();
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
